30a035bfb24ca2a80c1b08cd26b9d63ebeda941e
jonahkayizzi@gmail.com <jonahkayizzi@gmail.com>
2023-10-10 17:44:40 +0300

    Add login and register forms to the welcome page with toggling

--- agromocredit/screens/Welcome/WelcomeScreen.tsx
+++ agromocredit/screens/Welcome/WelcomeScreen.tsx
@@ -1,16 +1,62 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./WelcomeStyle";
-import ButtonLink from "../../components/Button/ButtonLink"
+import ButtonAction from "../../components/Buttons/ButtonAction"
+import InputText from "../../components/Inputs/InputText";
 
 const WelcomeScreen = () => {
+
+    const [activeForm, setActiveForm] = useState('buttons')
+
+    let bottomContent;
+
+    const handleChange = (newForm: React.SetStateAction<string>) => {
+        setActiveForm(newForm)
+    }
+
+    if(activeForm === 'buttons'){
+        bottomContent = (
+            <>
+            <Text style={styles.welcomeText} >WELCOME TO AGROMOCREDIT</Text>
+            <ButtonAction onPress={() => handleChange("register")} buttonText="Create Account" />
+            <ButtonAction onPress={() => handleChange("login")} buttonText="Login" />
+            </>
+        )
+    }else if(activeForm === "register"){
+        bottomContent = (
+            <>
+            <Text style={styles.welcomeText} >CREATE AN ACCOUNT</Text>
+            <InputText labelText="Email" />
+            <InputText labelText="MOMO Registered Number" />
+            <InputText labelText="Full Name" />
+            <InputText labelText="Password" />
+            <InputText labelText="Confirm Password" />
+            <Text style={styles.welcomeText} >Already have an account? </Text>
+            <ButtonAction onPress={() => handleChange("login")} buttonText="Login" />
+            </>
+        )
+    }else if (activeForm === 'login'){
+        bottomContent = (
+            <>
+            <Text style={styles.welcomeText} >SIGN IN</Text>
+            <InputText labelText="MOMO Registered Number" />
+            <InputText labelText="Password" />
+            <Text style={styles.welcomeText} >Not registered? </Text>
+            <ButtonAction onPress={() => handleChange("register")} buttonText="Create Account" />
+            </>
+        )
+    }
+
+
     return (
-        <SafeAreaView style={styles.container} > 
-            <Image source={require('../../assets/welcome.png')} style={styles.welcomeImage}  />
-            <Text style={styles.welcomeText} >Welcome to AgroMoCredit</Text>
-            <ButtonLink path="/create" linktext="Create Account" />
-            <ButtonLink path="/login" linktext="Login" />
+        <SafeAreaView style={styles.container} >
+            <TouchableOpacity onPress={() => handleChange("buttons")} > 
+                <Image source={require('../../assets/welcome.png')} style={styles.welcomeImage}  />
+            </TouchableOpacity>
+                <View>
+                {bottomContent}
+                </View>
         </SafeAreaView>
     )
 };
