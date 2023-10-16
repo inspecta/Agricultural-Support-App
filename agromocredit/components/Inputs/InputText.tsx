a6afefa9e1aa2470e59d3dc28fa68e7fdb6748ea
Derrick Mulinde <djmulinde@gmail.com>
2023-10-16 21:34:53 +0300

    Add input text props

--- agromocredit/components/Inputs/InputText.tsx
+++ agromocredit/components/Inputs/InputText.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { TextInput } from "react-native-paper";
+import React from "react"
+import { TextInput, View, Text } from "react-native"
 
 interface InputTextProps {
-    labelText: string;
+    labelText?: string
+    name?: string
     txtStyle: object;
   }
 
-const InputText:  React.FC<InputTextProps> = ({ labelText, txtStyle }) => {
+const InputText:  React.FC<InputTextProps> = ({ labelText, txtStyle, name, ...rest }) => {
     const [text, setText] = React.useState('');
-    return <TextInput style={txtStyle} label={labelText} placeholder={labelText} value={text}/>;
-    };
+    return (
+      <View>
+        {labelText && <Text>{labelText}</Text>}
+        <TextInput style={txtStyle} label={labelText} placeholder={labelText} value={text} {...rest}/>;
+      </View>
+    )
+  };
 
-export default InputText;
+export default InputText
