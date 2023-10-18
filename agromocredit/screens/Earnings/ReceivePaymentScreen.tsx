3c74dc3cd5938e41d167a92a2966670a76b718ba
Derrick Mulinde <djmulinde@gmail.com>
2023-10-18 21:22:35 +0300

    Add loading notification

--- agromocredit/screens/Earnings/ReceivePaymentScreen.tsx
+++ agromocredit/screens/Earnings/ReceivePaymentScreen.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react"
+import { Text, View, ActivityIndicator, StyleSheet } from "react-native"
 import InputText from "../../components/Inputs/InputText";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { IconButton, TextInput } from 'react-native-paper';
@@ -8,16 +8,124 @@ import ButtonAction from "../../components/Buttons/ButtonAction";
 import { screenStyles } from "../screenStyles";
 import {styles} from "./ReceivePaymentStyle" 
 import EarningsScreenHeaders from "../../components/Headers/EarningsScreenHeaders";
+import { useNavigation } from "@react-navigation/native"
+import axios from "axios"
+import generateNewReferenceId from "../../functions/GenerateReferenceId"
+import CustomModal from "../Notifications/CustomModal"
+import generateAccessToken from "../../functions/GenerateToken"
+import LoadingIndicator from "../Notifications/LoadingIndicator"
 
-const ReceivePaymentScreen = () => {
+const ReceivePaymentScreen: React.FC = ({ route }) => {
+  const [searchQuery, setSearchQuery] = React.useState("")
+  const [isLoading, setIsLoading] = React.useState(false)
 
-    const [searchQuery, setSearchQuery] = React.useState('');
+  const products = ["MILK", "ORANGES", "PINEAPPLES"]
 
-    const products = ["MILK", "ORANGES", "PINEAPPLES"];
+  const handleSelect = (selectedItem: any, index: number) => {
+    setSearchQuery(selectedItem)
+  }
 
-    const handleSelect = (selectedItem: any, index: number) => {
-        setSearchQuery(selectedItem);
-    };
+  const navigation = useNavigation()
+  const { user } = route.params
+
+  const [formValues, setFormValues] = useState({
+    payerNumber: "",
+    amount: "",
+    reason: "",
+  })
+
+  const handleInputChange = (name: string, value: string) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const [notificationVisible, setNotificationVisible] = useState(false)
+  const [transactionDetails, setTransactionDetails] = useState({
+    amount: "",
+    payerNumber: "",
+    reason: "",
+  })
+
+  const handleFormSubmit = async () => {
+    setIsLoading(true)
+
+    const newReferenceId = await generateNewReferenceId()
+    const accessToken = await generateAccessToken()
+
+    const formData = {
+      payerNumber: formValues.payerNumber,
+      amount: formValues.amount,
+      reason: formValues.reason,
+    }
+
+    const requestParams = {
+      amount: formData.amount,
+      currency: "EUR",
+      externalId: "string",
+      payer: {
+        partyIdType: "MSISDN",
+        partyId: formData.payerNumber,
+      },
+      payerMessage: formData.reason,
+      payeeNote: "MoMo",
+    }
+
+    const headers = {
+      "Ocp-Apim-Subscription-Key": "2820b39d5bc2421da2b3f42b4cce8929",
+      "X-Reference-Id": newReferenceId,
+      "X-Target-Environment": "sandbox",
+      Authorization: `Bearer ${accessToken}`,
+    }
+
+    try {
+      const response = await axios.post(
+        "https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay",
+        requestParams,
+        {
+          headers,
+        }
+      )
+
+      if (response.status === 202) {
+        const transactionData = {
+          amount: formData.amount,
+          description: formData.reason,
+          partyInvolved: formData.payerNumber,
+          transactionType: "Request Payment",
+          transactionDate: new Date().toISOString(),
+        }
+
+        setTransactionDetails({
+          amount: formValues.amount,
+          payerNumber: formValues.payerNumber,
+          reason: formValues.reason,
+        })
+
+        setNotificationVisible(true)
+
+        const txnResponse = await axios.post(
+          `http://192.168.1.117:8080/add-transaction?userId=${user.id}`,
+          transactionData
+        )
+
+        setIsLoading(false)
+      } else {
+        console.log("Could not complete transaction.")
+      }
+    } catch (error) {
+      console.error(error)
+      setIsLoading(false)
+    }
+  }
+
+  const handleOK = () => {
+    setNotificationVisible(false)
+    navigation.navigate("Dashboard", {
+      user,
+    })
+  }
 
     return (
         <SafeAreaView style={screenStyles.container}>
@@ -42,18 +150,37 @@ const ReceivePaymentScreen = () => {
                             }}/>
                     <IconButton icon="chevron-down" size={17} />
                 </View>
-                <InputText txtStyle={styles.textInput} labelText="From" />
-                <InputText txtStyle={styles.textInput} labelText="Amount" />
+                <InputText
+                    txtStyle={styles.textInput}
+                    labelText="From"
+                    name="payerNumber"
+                    value={formValues.payerNumber}
+                    onChangeText={(text) => handleInputChange("payerNumber", text)} 
+                />
+                <InputText 
+                    txtStyle={styles.textInput}
+                    labelText="Amount"
+                    name="amount"
+                    value={formValues.amount}
+                    onChangeText={(text) => handleInputChange("amount", text)}
+                />
                 <ButtonAction  
-                    onPress={() => console.log('Pressed')}
-                    buttonText="SEND"
+                    onPress={handleFormSubmit}
+                    buttonText="REQUEST"
                     buttonStyles={screenStyles.creditBtnStyles}
                     buttonTxtStyles={screenStyles.creditBtnTextStyles}
                 />
+              {/* Notification Modal */}
+              <CustomModal
+                visible={notificationVisible}
+                onClose={handleOK}
+                transactionDetails={transactionDetails}
+              />
             </View>
+            {isLoading && LoadingIndicator()}
         </SafeAreaView>
         
     );
     };
 
-export default ReceivePaymentScreen;
+export default ReceivePaymentScreen
