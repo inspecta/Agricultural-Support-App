3b5c33762bfe76b2f71722ba48a6b7f77ceb2bf6
Derrick Mulinde <djmulinde@gmail.com>
2023-10-19 12:06:31 +0300

    Add Login model
    - Model responsible for logging in

---
+++ agromocredit-backend/src/main/java/com/backend/agriculturalsupportapp/model/Login.java
@@ -0,0 +1,13 @@
+package com.backend.agriculturalsupportapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Login {
+    private String phoneNumber;
+    private String password;
+}
