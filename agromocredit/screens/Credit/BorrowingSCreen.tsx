ddf7c081f41a6da2200ddbc493d5baca4b73fccb
Derrick Mulinde <djmulinde@gmail.com>
2023-10-22 17:42:50 +0300

    Add borrow loans functionality

--- agromocredit/screens/Credit/BorrowingSCreen.tsx
+++ agromocredit/screens/Credit/BorrowingSCreen.tsx
@@ -1,57 +1,161 @@
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import CreditScreenHeader from "../../components/Headers/CreditScreenHeader";
-import { View, Text } from "react-native";
-import { IconButton } from "react-native-paper";
-import InputText from "../../components/Inputs/InputText";
-import ButtonAction from "../../components/Buttons/ButtonAction";
-import { screenStyles } from "../screenStyles";
-import TransactionRecord from "../../components/TransactionRecord";
+import React, { useState, useEffect } from "react"
+import { SafeAreaView } from "react-native-safe-area-context"
+import CreditScreenHeader from "../../components/Headers/CreditScreenHeader"
+import { View, Text } from "react-native"
+import InputText from "../../components/Inputs/InputText"
+import ButtonAction from "../../components/Buttons/ButtonAction"
+import { screenStyles } from "../screenStyles"
+import TransactionRecord from "../../components/TransactionRecord"
+import LoadingIndicator from "../Notifications/LoadingIndicator"
+import CustomModal from "../Notifications/CustomModal"
+import { useNavigation } from "@react-navigation/native"
+import axios from "axios"
 
-const BorrowingScreen = () => {
-    return (
-        <SafeAreaView style={screenStyles.creditScreenContainer}>
-            <CreditScreenHeader screenTitle="CREDIT STATUS" activeButton="borrow" />
-            <View style={screenStyles.contentContainer}>
-                <Text style={screenStyles.creditScreenSubTitleText}>OUTSTANDING CREDIT</Text>
-                <Text style={screenStyles.creditScreenMajorText}>- UGX 567,000</Text>
-                <View style={screenStyles.recordContainer}>
-                    <TransactionRecord
-                        recordDate="15 SEP"
-                        recordValue="MULUNDO SAM"
-                        recordIcon=""
-                        recordSubject="WEED MASTER"
-                        recordSubAttr1="UGX 498,000"
-                        recordSubAttr2="UGX 500,000"
-                        recordDated={true}
-                        detailsIcon={false}
-                        creditScreen={true}    
-                    />
-                    <TransactionRecord
-                        recordDate="15 JUL"
-                        recordValue="MOMO"
-                        recordIcon=""
-                        recordSubject="GENERAL"
-                        recordSubAttr1="UGX 50,000"
-                        recordSubAttr2="UGX 500,000"
-                        recordDated={true}
-                        detailsIcon={false}
-                        creditScreen={true}    
-                    />
-                </View>
-                <View style={screenStyles.creditScreenSubTitleText}>
-                    <Text>AMOUNT</Text>
-                    <InputText txtStyle={screenStyles.creditScreenTextInput} labelText="UGX 450,000" />
-                    <ButtonAction
-                        onPress={() => console.log('Pressed')}
-                        buttonText="BORROW"
-                        buttonStyles={screenStyles.creditBtnStyles}
-                        buttonTxtStyles={screenStyles.creditBtnTextStyles}
-                    />
-                </View>
-            </View>
-        </SafeAreaView>
-    );
-};
+interface User {
+  name: string
+  balance: number
+  id: number
+  phoneNumber: string
+}
 
-export default BorrowingScreen;
+interface DashboardScreenProps {
+  route: {
+    params: {
+      user: User
+    }
+  }
+}
+
+const BorrowingScreen: React.FC<DashboardScreenProps> = ({ route }) => {
+  const { user } = route.params
+  const navigation = useNavigation()
+
+  const [creditScore, setCreditScore] = useState(0)
+  const [borrowAmount, setBorrowAmount] = useState("")
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [notificationVisible, setNotificationVisible] = useState(false)
+  const [transactionDetails, setTransactionDetails] = useState({
+    amount: "",
+    loanProvider: "",
+    reason: "",
+  })
+
+  /** Maximum amount a user can borrow at the moment */
+  const MAXIMUM_BORROWING_AMOUNT = 1000000
+
+  useEffect(() => {
+    const fetchCreditScore = async () => {
+      try {
+        const response = await axios.get(
+          `http://192.168.9.200:8080/calculate-credit-score/${user.id}`
+        )
+        setCreditScore(response.data)
+      } catch (error) {
+        console.error("Error fetching credit score:", error)
+      }
+    }
+
+    fetchCreditScore()
+  }, [user.id])
+
+  /** The maximum loan a user can get at the moment */
+  const user_maximum_amount = (
+    (creditScore / 10) *
+    MAXIMUM_BORROWING_AMOUNT
+  ).toLocaleString()
+
+  /*** Handle borrowing money */
+  const handleBorrowing = async () => {
+    if (+borrowAmount < +user_maximum_amount && +borrowAmount > 10000) {
+      console.log("Cannot borrow more than " + user_maximum_amount)
+    } else {
+      setIsLoading(true)
+
+      try {
+        const saveLoanUrl = `http://192.168.9.200:8080/save-loan/${user.id}`
+        const response = await axios.post(saveLoanUrl, {
+          amount: borrowAmount,
+          interest_rate: 6,
+          loan_provider_id: 2,
+          user_id: user.id,
+        })
+
+        if (response.status === 200) { 
+          setIsLoading(false)
+
+          setTransactionDetails({
+            amount: borrowAmount,
+            loanProvider: "AGROMOCREDIT",
+            reason: "LOAN",
+          })
+
+          setNotificationVisible(true)
+        }
+      } catch (error) {
+        console.error("Error saving loan:", error)
+        setIsLoading(false)
+      }
+    }
+  }
+
+  const handleOK = () => {
+    setNotificationVisible(false)
+    navigation.navigate("Dashboard", {
+      user: user,
+    })
+  }
+
+  return (
+    <SafeAreaView style={screenStyles.creditScreenContainer}>
+      <CreditScreenHeader screenTitle="CREDIT STATUS" activeButton="borrow" />
+      <View style={screenStyles.contentContainer}>
+        <Text style={screenStyles.creditScreenSubTitleText}>
+          YOU CAN BORROW UP TO
+        </Text>
+        <Text style={screenStyles.creditScreenMajorText}>
+          UGX {user_maximum_amount}{" "}
+        </Text>
+        <Text style={{ fontSize: 13, color: "red" }}>Interest Rate: 6%</Text>
+
+        <View style={screenStyles.recordContainer}>
+          <TransactionRecord
+            recordDate="15 SEP"
+            recordValue="MULUNDO SAM"
+            recordIcon=""
+            recordSubject="WEED MASTER"
+            recordSubAttr1="UGX 498,000"
+            recordSubAttr2="UGX 500,000"
+            recordDated={true}
+            detailsIcon={false}
+            creditScreen={true}
+          />
+        </View>
+        <View style={screenStyles.creditScreenSubTitleText}>
+          <Text>ENTER AMOUNT TO BORROW</Text>
+          {/* {errorMessage && <Text>{errorMessage}</Text>} */}
+          <InputText
+            txtStyle={screenStyles.creditScreenTextInput2}
+            labelText="Amount you want to borrow!"
+            value={borrowAmount}
+            onChangeText={setBorrowAmount}
+          />
+          <ButtonAction
+            onPress={() => handleBorrowing()}
+            buttonText="BORROW"
+            buttonStyles={screenStyles.creditBtnStyles}
+            buttonTxtStyles={screenStyles.creditBtnTextStyles}
+          />
+        </View>
+        {/* Notification Modal */}
+        <CustomModal
+          visible={notificationVisible}
+          onClose={handleOK}
+          transactionDetails={transactionDetails}
+        />
+      </View>
+      {isLoading && LoadingIndicator()}
+    </SafeAreaView>
+  )
+}
+
+export default BorrowingScreen
