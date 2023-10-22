56cc6e522c3f918af5725c397ac9b48f5727a72e
jonahkayizzi@gmail.com <jonahkayizzi@gmail.com>
2023-10-22 09:12:26 +0300

    Add a provider for the store to the app

--- agromocredit/App.tsx
+++ agromocredit/App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import WelcomeScreen from "./screens/Welcome/WelcomeScreen"
 import DashboardScreen from "./screens/Dashboard/DashboardScreen"
@@ -12,28 +11,33 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import WithdrawScreenal from "./screens/Transactions/WithdrawalScreen"
 import WithdrawalsScreen from "./screens/Transactions/WithdrawalsScreen"
+import {Provider} from "react-redux";
+import { store } from "./services/store"
 
 const Stack = createStackNavigator()
 
 export default function App() {
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        <Stack.Screen
-          name="ReceivePaymentScreen"
-          component={ReceivePaymentScreen}
-        />
-        <Stack.Screen name="Withdraw" component={WithdrawScreenal} />
-        <Stack.Screen name="Withdraws" component={WithdrawalsScreen} />
-        <Stack.Screen name="Borrowing" component={BorrowingScreen} />
-        <Stack.Screen name="PayLoan" component={PayLoanScreen} />
-        <Stack.Screen name="Earnings" component={EarningsScreen} />
-        <Stack.Screen name="Credit" component={CreditScreen} />
-        <Stack.Screen name="LentOut" component={LentOutScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Dashboard" component={DashboardScreen} />
+          <Stack.Screen
+            name="ReceivePaymentScreen"
+            component={ReceivePaymentScreen}
+          />
+          <Stack.Screen name="Withdraw" component={WithdrawScreenal} />
+          <Stack.Screen name="Withdraws" component={WithdrawalsScreen} />
+          <Stack.Screen name="Borrowing" component={BorrowingScreen} />
+          <Stack.Screen name="PayLoan" component={PayLoanScreen} />
+          <Stack.Screen name="Earnings" component={EarningsScreen} />
+          <Stack.Screen name="Credit" component={CreditScreen} />
+          <Stack.Screen name="LentOut" component={LentOutScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
 
