3e956a60ab051d79e264b14756ae513d30b3e1f1
Derrick Mulinde <djmulinde@gmail.com>
2023-10-25 17:38:29 +0300

    Update placeholder color

--- agromocredit/components/Inputs/InputText.tsx
+++ agromocredit/components/Inputs/InputText.tsx
@@ -23,7 +23,7 @@ const InputText: React.FC<InputTextProps> = ({
       onChangeText={onChangeText}
       style={txtStyle}
       placeholder={labelText}
-      placeholderTextColor="#fff"
+      placeholderTextColor="rgba(255, 255, 255, 0.8)"
       value={value}
       {...rest}
     />
