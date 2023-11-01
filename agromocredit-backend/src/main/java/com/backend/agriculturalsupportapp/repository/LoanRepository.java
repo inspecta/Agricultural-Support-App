dfeed8b0b48a143be107e953967039d460f4f3c6
Derrick Mulinde <djmulinde@gmail.com>
2023-11-01 17:42:08 +0300

    Delete agromocredit-backend directory

--- agromocredit-backend/src/main/java/com/backend/agriculturalsupportapp/repository/LoanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.agriculturalsupportapp.repository;
-
-import com.backend.agriculturalsupportapp.model.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-}
